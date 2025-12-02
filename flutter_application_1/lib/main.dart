48c49670d51c1235b4e6e9f11a8053b7cc79f314
Kaz-Ed <164074624+Kaz-Ed@users.noreply.github.com>
2025-12-02 14:18:34 +0300

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/login_screen.dart';
-import 'screens/main_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Загружаем переменные окружения
+  await dotenv.load(fileName: ".env");
+  
+  // Инициализируем Supabase
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final supabase = Supabase.instance.client;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
